753edc2aff8058e5f2f01e8ed30d2b2de8950489
abidmuin <muin.739@gmail.com>
2023-10-28 12:04:33 +0600

    Implemented all flags

---
+++ wc-tool/README.md
@@ -0,0 +1,3 @@
+- [Problem Link](https://codingchallenges.fyi/challenges/challenge-wc/)
+
+- [wc tool man page](https://www.gnu.org/software/coreutils/manual/html_node/wc-invocation.html#wc-invocation/)
---
+++ wc-tool/python/ccwc.py
@@ -0,0 +1,78 @@
+import sys
+import argparse
+
+
+def count_bytes(file_content):
+	byte_count = len(file_content)
+	return byte_count
+
+
+def count_lines(file_content):
+	line_count = file_content.count('\n')
+	return line_count
+
+
+def count_words(file_content):
+	words = file_content.split()
+	words_count = len(words)
+	return words_count
+
+
+def count_characters(file_content):
+	char_count = len(file_content)
+	return char_count
+
+
+def main():
+	parser = argparse.ArgumentParser(prog='Custom Word Count Tool',
+	                                 description='Print newline, word, and byte counts for each FILE, and a total line if more than one FILE is specified. With no FILE, or when FILE is -, read standard input.',
+	                                 epilog='Text at the bottom of help')
+	parser.add_argument('-c', '--bytes', action='store_true', help='print the byte counts')
+	parser.add_argument('-l', '--lines', action='store_true', help='print the newline counts')
+	parser.add_argument('-w', '--words', action='store_true', help='print the word counts')
+	parser.add_argument('-m', '--chars', action='store_true', help='print the character counts')
+	parser.add_argument('file', metavar='file', type=str, nargs='?', help='File path')
+	args = parser.parse_args()
+
+	if args.file:
+		with open(args.file, 'rb') as file:
+			file_content = file.read()
+	else:
+		file_content = sys.stdin.read()
+
+	if args.bytes:
+		result = count_bytes(file_content)
+		if args.file:
+			print(f'{result} {args.file}')
+		else:
+			print(result)
+	elif args.lines:
+		result = count_lines(file_content)
+		if args.file:
+			print(f'{result} {args.file}')
+		else:
+			print(result)
+	elif args.words:
+		result = count_words(file_content)
+		if args.file:
+			print(f'{result} {args.file}')
+		else:
+			print(result)
+	elif args.chars:
+		result = count_characters(file_content)
+		if args.file:
+			print(f'{result} {args.file}')
+		else:
+			print(result)
+	else:
+		bytes_count = count_bytes(file_content)
+		lines_count = count_lines(file_content)
+		words_count = count_words(file_content)
+		if args.file:
+			print(f'{bytes_count} {lines_count} {words_count} {args.file}')
+		else:
+			print(f'{bytes_count} {lines_count} {words_count}')
+
+
+if __name__ == '__main__':
+	main()
