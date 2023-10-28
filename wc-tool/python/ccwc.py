2acbb5743beea5b3d10796a802584af98ddbf768
abidmuin <muin.739@gmail.com>
2023-10-28 21:35:23 +0600

    v1.0.0 - Challenge Completed

--- wc-tool/python/ccwc.py
+++ wc-tool/python/ccwc.py
@@ -8,25 +8,28 @@ def count_bytes(file_content):
 
 
 def count_lines(file_content):
-	line_count = file_content.count('\n')
+	line_count = file_content.count(b'\n')
 	return line_count
 
 
 def count_words(file_content):
-	words = file_content.split()
+	words = file_content.decode('utf-8').split()
 	words_count = len(words)
 	return words_count
 
 
 def count_characters(file_content):
-	char_count = len(file_content)
+	char_count = len(file_content.decode('utf-8'))
 	return char_count
 
 
 def main():
-	parser = argparse.ArgumentParser(prog='Custom Word Count Tool',
-	                                 description='Print newline, word, and byte counts for each FILE, and a total line if more than one FILE is specified. With no FILE, or when FILE is -, read standard input.',
-	                                 epilog='Text at the bottom of help')
+	parser = argparse.ArgumentParser(
+		prog='Custom Word Count Tool by @abidmuin',
+		description='Print bytes, lines, words and characters counts for each FILE',
+		epilog='Author: https://github.com/abidmuin',
+		formatter_class=argparse.RawDescriptionHelpFormatter,
+	)
 	parser.add_argument('-c', '--bytes', action='store_true', help='print the byte counts')
 	parser.add_argument('-l', '--lines', action='store_true', help='print the newline counts')
 	parser.add_argument('-w', '--words', action='store_true', help='print the word counts')
@@ -37,8 +40,11 @@ def main():
 	if args.file:
 		with open(args.file, 'rb') as file:
 			file_content = file.read()
+	elif not sys.stdin.isatty():
+		file_content = sys.stdin.buffer.read()
 	else:
-		file_content = sys.stdin.read()
+		print("No standard input found.")
+		sys.exit(1)
 
 	if args.bytes:
 		result = count_bytes(file_content)
